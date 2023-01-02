7126c7c56868dd92ef09a0da9212b306a759c6c1
andrew karppinen <99529988+andrew-karppinen@users.noreply.github.com>
2023-01-02 21:11:25 +0200

    Add files via upload

--- esimerkki.py
+++ esimerkki.py
@@ -1,24 +1,30 @@
 import pygame
 
-from object_class import Objekti
+from PygameSimpleObject import Objekti, TunnistaTormays
 
 pygame.init() #alustetaan pygame moduuli
-naytto = pygame.display.set_mode((640, 480)) #luodaan ikkuna
+naytto = pygame.display.set_mode((850, 700)) #luodaan ikkuna
 pygame.display.set_caption("pygame") #nimetään ikkuna
 
 
 robottikuva = pygame.image.load("kuvat/robotti.png") #ladataan kuva robotti
 tasokuva = pygame.image.load("kuvat/taso.png")
+maalikuva = pygame.image.load("kuvat/maali.png")
 
 #luodaan kello jolla rajoitetaan pelin nopeutta
 kello = pygame.time.Clock()
 
 
-robotti = Objekti(robottikuva,sijainti_x=400,sijainti_y=100,painovoiman_voimakkuus=0.5,hypyn_voimakkuus=15)
-taso = Objekti(tasokuva,sijainti_x=100,sijainti_y=400)
+robotti = Objekti(robottikuva,sijainti_x=200,sijainti_y=100,painovoiman_voimakkuus=0.5,hypyn_voimakkuus=15)
+taso = Objekti(tasokuva,sijainti_x=100,sijainti_y=600)
+taso2 = Objekti(tasokuva,sijainti_x=600,sijainti_y=400)
+maali = Objekti(maalikuva,sijainti_x=700,sijainti_y=335)
 
 robotti.LisaaTormays(taso)
+robotti.LisaaTormays(taso2)
 taso.LisaaTormays(robotti)
+
+
 
 tasovasemmalle = False
 vasemmalle = False
@@ -50,20 +56,17 @@ while True: #pääsilmukka
                 vasemmalle = False
             if tapahtuma.key == pygame.K_RIGHT:
                 oikealle = False
-    
-
 
         if tapahtuma.type == pygame.QUIT: #jos ohjelma suljetaan
             exit() 
         
 
     if oikealle:
-        robotti.LiikuX(2)
+        robotti.LiikuX(4)
     if vasemmalle:
-        robotti.LiikuX(-2)
+        robotti.LiikuX(-4)
     if alas:
-        robotti.LiikuY(2)
-
+        robotti.LiikuY(4)
 
 
     if taso.sijainti_x_ == 500:
@@ -78,9 +81,15 @@ while True: #pääsilmukka
         
     robotti.Painovoima()
     
+    if TunnistaTormays(robotti,maali):
+        robotti.sijainti_x_ = 200
+        robotti.sijainti_y_ = 100
+    
     naytto.fill((0,0,0)) #täytetään ruutu mustalla
     naytto.blit(robotti.kuva_,(robotti.sijainti_x_,robotti.sijainti_y_))
     naytto.blit(taso.kuva_,(taso.sijainti_x_,taso.sijainti_y_))
+    naytto.blit(taso2.kuva_,(taso2.sijainti_x_,taso2.sijainti_y_))
+    naytto.blit(maali.kuva_,(maali.sijainti_x_,maali.sijainti_y_))
 
     pygame.display.flip() #päivittää näytön
     
