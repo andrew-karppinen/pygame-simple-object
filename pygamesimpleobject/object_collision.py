ca9aca6f14be650ad8eeaed464dcc4bc63dea2ea
ak <awkk12345@gmail.com>
2024-03-05 19:25:12 +0200

    CollisionCheck can be passed a list instead of a single object.

--- pygamesimpleobject/object_collision.py
+++ pygamesimpleobject/object_collision.py
@@ -9,7 +9,38 @@ def CollisionCheck(obj1, obj2, obj1_x = None, obj1_y = None, obj2_x = None, obj2
     '''
 
 
-    
+    if type(obj1) == list or type(obj2) == list: #if obj1 or obj2 is list
+        if type(obj1) == list: #obj1 is list
+            for obj in obj1:
+
+                if type(obj2) != list: #obj2 is not list
+                    if obj2_x != None and obj2_y != None: #obj2 position is given
+                        if CollisionCheck(obj, obj2,obj2_x = obj2_x,obj2_y=obj2_y) == True:
+                            return True
+                    else: #obj2 position is no given
+                        if CollisionCheck(obj, obj2) == True:
+                            return True
+                else: #obj2 is list
+                    if CollisionCheck(obj, obj2) == True:
+                        return True
+
+
+        if type(obj2) == list: #obj2 is list
+            for obj in obj2:
+
+                if type(obj1) != list: #obj1 is not list
+                    if obj1_x != None and obj1_y != None: #obj1 position is given
+                        if CollisionCheck(obj, obj1,obj2_x = obj1_x,obj2_y=obj1_y) == True:
+                            return True
+                    else: #obj1 position is no given
+                        if CollisionCheck(obj, obj1) == True:
+                            return True
+
+                if CollisionCheck(obj,obj1) == True:
+                    return True
+
+        return False
+
     if obj1_x == None: #if location no given
         obj1_x = obj1.position_x_
     if obj1_y == None: #if location no given
@@ -28,7 +59,6 @@ def CollisionCheck(obj1, obj2, obj1_x = None, obj1_y = None, obj2_x = None, obj2
             if obj1_x < obj2_x + obj2.object_size_x_:
                 if obj1_x + obj1.object_size_x_ > obj2_x:
                     return True
-
     return False
 
 
