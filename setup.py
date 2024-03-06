9e552846a943735b053386a42b578e18f4146422
andrew karppinen <99529988+andrew-karppinen@users.noreply.github.com>
2024-03-06 14:40:08 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pygame-simple-object',
+    version='',
+    packages=['pygamesimpleobject'],
+    url='',
+    license='',
+    author='ak',
+    author_email='',
+    description=''
+)
